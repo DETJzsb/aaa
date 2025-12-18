24a14972fa4fbbdf7b0815c28ece665b8a9e7df3
ZSB <altlili.aymen@gmail.com>
2025-12-18 15:33:59 +0100

    Update supabase-admin.js

--- admin/js/supabase-admin.js
+++ admin/js/supabase-admin.js
@@ -3,4 +3,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 export const supabase = createClient(
   "https://wiovumauoaxrrrsjwkko.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpb3Z1bWF1b2F4cnJyc2p3a2tvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTc2NTIsImV4cCI6MjA4MTEzMzY1Mn0._wg6or6Ht00JRtNAYkv5MNHjtocMLNPg2-StMj4Ul68"
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
