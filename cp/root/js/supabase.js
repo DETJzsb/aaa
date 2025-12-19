846200b5dc922ea0dc24b837639cfc52e01c5fe9
ZSB <altlili.aymen@gmail.com>
2025-12-20 00:25:37 +0100

    Create supabase.js

---
+++ cp/root/js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+
+export const supabase = createClient(
+  "https://wiovumauoaxrrrsjwkko.supabase.co",
+  "sb_publishable_RR7nRUaRjq0qnAm1r0H1sg_HOauofxw"
+)
