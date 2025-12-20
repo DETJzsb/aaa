adcef6f4aaf2a70463ebd977ac90ea3a46aefb05
ZSB <altlili.aymen@gmail.com>
2025-12-20 15:32:08 +0100

    Update profile.js

--- cp/root/js/profile.js
+++ cp/root/js/profile.js
@@ -1,18 +1,14 @@
 import { supabase } from "./supabase.js"
 
-async function loadProfile() {
-  const { data: { session } } = await supabase.auth.getSession()
+const { data: { session } } = await supabase.auth.getSession()
 
-  document.getElementById("p_name").textContent =
-    session.user.user_metadata?.nom || "Root Admin"
+document.getElementById("name").textContent =
+  session.user.user_metadata?.nom || "Root"
 
-  document.getElementById("p_email").textContent = session.user.email
-  document.getElementById("p_id").textContent = session.user.id
-}
+document.getElementById("email").textContent = session.user.email
+document.getElementById("id").textContent = session.user.id
 
 window.logout = async () => {
   await supabase.auth.signOut()
   window.location.replace("login.html")
 }
-
-loadProfile()
