833d4bbfc4fb1704ba5db821d4c12048f1a7a201
ZSB <altlili.aymen@gmail.com>
2025-12-20 16:01:36 +0100

    Update adduser.js

--- cp/root/js/adduser.js
+++ cp/root/js/adduser.js
@@ -9,7 +9,8 @@ window.addUser = async () => {
     "https://wiovumauoaxrrrsjwkko.supabase.co/functions/v1/add-user",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" ,
+        "Authorization": `Bearer ${session.access_token}` },
       body: JSON.stringify(body)
     }
   )
