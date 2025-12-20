ee0ff9d44d50d209c74403ade9da94f1ea33a9d0
ZSB <altlili.aymen@gmail.com>
2025-12-20 15:29:54 +0100

    Update suppuser.js

--- cp/root/js/suppuser.js
+++ cp/root/js/suppuser.js
@@ -1,28 +1,14 @@
-import { getJwt } from "./supabase.js"
-import { loadUsers } from "./loadusers.js"
-
 window.deleteUser = async (id) => {
-  if (!confirm("Delete this user?")) return
+  if (!confirm("Delete user?")) return
 
-  const token = await getJwt()
-
-  const res = await fetch(
+  await fetch(
     "https://wiovumauoaxrrrsjwkko.supabase.co/functions/v1/delete-user",
     {
       method: "POST",
-      headers: {
-        "Authorization": `Bearer ${token}`,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ userId: id })
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id })
     }
   )
 
-  const data = await res.json()
-
-  if (!res.ok) {
-    alert(data.error)
-  } else {
-    loadUsers()
-  }
+  location.reload()
 }
