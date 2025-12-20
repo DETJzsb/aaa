c388de79b90405cd48e3982bf33589bdcb0e27f5
ZSB <altlili.aymen@gmail.com>
2025-12-20 15:26:02 +0100

    Update redirect.js

--- cp/root/js/redirect.js
+++ cp/root/js/redirect.js
@@ -1,22 +1,20 @@
 import { supabase } from "./supabase.js"
 
-async function protect() {
+const protect = async () => {
   const { data: { session } } = await supabase.auth.getSession()
 
-  // مش logged
   if (!session) {
     window.location.replace("login.html")
     return
   }
 
-  // مش root
-  const { data, error } = await supabase
+  const { data } = await supabase
     .from("root")
     .select("id")
     .eq("id", session.user.id)
     .single()
 
-  if (error || !data) {
+  if (!data) {
     await supabase.auth.signOut()
     window.location.replace("login.html")
   }
