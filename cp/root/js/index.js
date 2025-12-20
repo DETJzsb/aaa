8283a486dceae7079cb266425fcf4b4bc35a680f
ZSB <altlili.aymen@gmail.com>
2025-12-20 16:46:38 +0100

    Update index.js

--- cp/root/js/index.js
+++ cp/root/js/index.js
@@ -1,28 +1,5 @@
-import { supabase } from "./supabase.js"
+import { loadUsers } from "./loadusers.js"
 
-async function loadUsers() {
-  const { data } = await supabase.from("users").select("*")
-
-  const table = document.getElementById("users")
-  table.innerHTML = `
-    <tr>
-      <th>Name</th><th>Email</th><th>Role</th><th>Actions</th>
-    </tr>
-  `
-
-  data.forEach(u => {
-    table.innerHTML += `
-      <tr>
-        <td>${u.nom}</td>
-        <td>${u.email}</td>
-        <td>${u.role}</td>
-        <td>
-          <button onclick="deleteUser('${u.id}')">❌</button>
-          <button onclick="resetPw('${u.id}','${u.email}')">🔑</button>
-        </td>
-      </tr>
-    `
-  })
+window.onload = () => {
+  loadUsers()
 }
-
-window.onload = loadUsers
