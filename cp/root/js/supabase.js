f661a723d019a74ae60afddcae45ca9461658911
ZSB <altlili.aymen@gmail.com>
2025-12-20 01:53:13 +0100

    Update supabase.js

--- cp/root/js/supabase.js
+++ cp/root/js/supabase.js
@@ -4,3 +4,17 @@ export const supabase = createClient(
   "https://wiovumauoaxrrrsjwkko.supabase.co",
   "sb_publishable_RR7nRUaRjq0qnAm1r0H1sg_HOauofxw"
 )
+
+// ترجع JWT أو ترجع للـ login
+export async function getJwt() {
+  const {
+    data: { session }
+  } = await supabase.auth.getSession()
+
+  if (!session) {
+    window.location.href = "login.html"
+    throw new Error("Not authenticated")
+  }
+
+  return session.access_token
+}
